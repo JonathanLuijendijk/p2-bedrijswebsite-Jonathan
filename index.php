efcd444040c81794380d4313312751c3d39dd988
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2021-10-29 19:22:53 +0200

    Sushi is lekker

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,123 +12,137 @@
 
 <body>
     <header class="banner">
-    <img src="image/logo.jpg" class="logo" alt="ShabutoGo" width="200" height="200" style="max-width:100%;height:auto;">
-    <?php     include 'navigatie.html';    ?>
+      <img src="image/logo.jpg" alt="logo" width="200" height="200" style="max-width: 100%;height: auto;">
+      <?php     include 'navigatie.html';    ?>
     </header>
 
     <main>
-        <section>
-            <article>
-            <img src="image/shabu-to-go-nijmegen-20112010411334.jpg" alt="sushi" style="max-width:100%;height:auto;">
-                <h1>Welkom</h1>
-                <p>Bij dit website kun je lekker eten gaan bestellen.<br>
-                   Je kan ook zien waar Shabu to go in Nederland ligt en wanneer tot wanneer het open is.</p>
-                   </article>
-        </section>
-
-        <section>
-            <article>
-                   <p><big>LET OP!</big><br>
-                   Dit website is bestemd bij Shabu to Go in Spijkenisse!</p>
-            </article>
-        </section>
+      <section>
+        <article>
+        <img src="image/6c2065f3-8020-4f51-ac6d-50d81ffdc8c2.png" alt="sushi" style="max-width:100%;height:auto;">
+            <h1>Welkom</h1>
+            <p>Bij dit website kun je lekkerste sushi gaan bestellen.<br>
+               Je kan ook zien waar Shabu to go in Nederland ligt en wanneer tot wanneer het open is.</p>
+               </article>
+    </section>
 
     <section>
         <article>
-          <table>
-            <caption>
-              <p>Hier kun je de tijden zien</p>
-            </caption>
-            <tbody>
-              <tr>
-                <td>Maandag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Dinsdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Woensdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Donderdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Vrijdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Zaterdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Zondag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-            </tbody>
-          </table>
-          </article>
+               <p><big>LET OP!</big><br>
+               Dit website is bestemd bij Shabu to Go in Spijkenisse!</p>
+        </article>
+    </section>
+
+<section>
+    <article>
+      <table>
+        <caption>
+          <p>Hier kun je de tijden zien</p>
+        </caption>
+        <tbody>
+          <tr>
+            <td>Maandag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Dinsdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Woensdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Donderdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Vrijdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Zaterdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Zondag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+        </tbody>
+      </table>
+      </article>
 </section>
- 
+
+<section>
+<article>
+<img src="image/shabu-to-go-nijmegen-20112010411334.jpg" alt="Shabu" style="max-width:100%;height:auto;">
+      <h2>Wanneer bezorgen jullie?</h2>
+      <p> de bezorg tijden staat als volgt.</p>
+        <table>
+         <tr>
+            <td>Maandag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Dinsdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Woensdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Donderdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Vrijdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Zaterdag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+          <tr>
+            <td>Zondag</td>
+            <td>12:00 - 21:00</td>
+          </tr>
+        </tbody>
+      </table>
+      </article>
+</section>
+
 <section>
   <article>
-    <img src="image/d5a25da4-d723-4695-a9ae-84fb8a2f57dd.png" alt="Shabu" style="max-width:100%;height:auto;">
-          <h2>Bezorgen jullie ook eten?</h2>
-          <p>Ja, wij bezorgen ook eten. <br>
-             de volgende tijden staat als volgt.</p>
-            <table>
-             <tr>
-                <td>Maandag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Dinsdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Woensdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Donderdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Vrijdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Zaterdag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-              <tr>
-                <td>Zondag</td>
-                <td>12:00 - 21:00</td>
-              </tr>
-            </tbody>
-          </table>
-          </article>
+    <h2>Wij zijn SHABU to go: de bezorg- en afhaaldienst van overheerlijke sushi!</h2>
+    <p>Bij SHABU to go kun je online lekkere sushi bestellen.
+       We leveren een uitstekende service en werken met verse producten,<br>
+       waardoor je ook thuis kunt genieten van onze heerlijke gerechten.</p>
+
+       <p>Zowel de smaak van de producten als de kwaliteit van onze ingrediënten is belangrijk en staat bij ons hoog in het vaandel.<br>
+        Elke dag worden de gerechten klaargemaakt met vers geleverde producten en leveren wij een uitgebreid assortiment aan sushi en verschillende bijgerechten.</p>
+       
+        <p>De lekkerste sushi bestellen bij SHABU to go doe je vanuit je luie stoel.<br>
+        Onze bezorgers zorgen ervoor dat je binnen no time kunt genieten van je sushi!</p>
+      </article>
 </section>
 
 
 <section>
-  <article>
-  <h2>Vacature scooter</h2>
-  <p>Kom bij ons werken! Wij zoeken nu mensen om eten te bezorgen.</p>
-  <img src="image/Bezorgers_banner.png" alt="bezorgen" style="max-width:100%;height:auto;">  
-  </article>
+<article>
+<h2>Vacature scooter</h2>
+<p>Sluit bij ons aan! Wij zoeken nu mensen om lekker eten te bezorgen.</p>
+<img src="image/Bezorgers_banner.png" alt="bezorgen" style="max-width:100%;height:auto;">  
+</article>
 </section>
 </main>
 
-    <footer>
-        <p><i>Nieuwsstraat 210<br>
-               3201 EE<br>
-               Spijkenisse</i>
-        </p>
-    </footer>
+<footer>
+    <p><i>Nieuwsstraat 210<br>
+           3201 EE<br>
+           Spijkenisse</i>
+    </p>
+</footer>
 
 </body>
 
