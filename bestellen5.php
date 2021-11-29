e4e54ac93ae92737fd76ba84639a4e09e055dc3a
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2021-11-29 13:33:39 +0100

    Commit 1

--- bestellen5.php
+++ bestellen5.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bestellen</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <link rel="stylesheet" type="text/css" href="styles].css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
