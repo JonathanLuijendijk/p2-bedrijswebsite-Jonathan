b7a2ab5a6446957e8ab99327e9e70921e4996427
Brandonvde010 <brandonvanderent010@gmail.com>
2021-11-29 13:32:09 +0100

    openingstijden

---
+++ openingstijden.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>openingstijden</title>
+</head>
+<body>
+<header class="banner">
+        <img src="image/logo.jpg" alt="logo" width="200" height="200">
+        <?php include 'navigatie.html'; ?>
+    </header>
+</body>
+</html>
