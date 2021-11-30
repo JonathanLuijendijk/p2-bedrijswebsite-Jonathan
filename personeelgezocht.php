36241d5229d8e6ecdaeafe10463face9cfa044c4
robinjcp <90386373+robinjcp@users.noreply.github.com>
2021-11-30 11:26:08 +0100

    Add files via upload

---
+++ personeelgezocht.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shabu to GO</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header class="banner">
+    <img src="image/logo.jpg" alt="logo" width="200" height="200">
+    <?php   include 'navigatie.html'; ?>
+  </header>
+
+<main>
+  <section>
+    <article>
+      <h1>
+        Welkom bij de medewerkers gezocht pagina!, wij zoeken JOU kom jij ons gezellige team versterken?
+      </h1>
+      <p>Bij Shabu To Go kan je werken als kok, bezorger, schoonmaker noem maar op, er is genoeg te doen.</p>
+      <p>Met ons gezellige team is werken leuker dan ooit, met flexibele werktijden vrije vakanties en veel meer is dit de leukste baan van nederland. Ben jij 16 jaar of ouder?, dan kan je bezorgen met een scooter van Shabu To Go!</p>
+    </article>
+  </section>
+
+  <section>
+<article>
+<h2>Vacature scooter</h2>
+<p>Sluit bij ons aan! Wij zoeken nu mensen om lekker eten te bezorgen.</p>
+<a href="https://www.shabutogo.nl/vacatures"></a>
+<img src="image/Bezorgers_banner.png" alt="bezorgen">
+</article>
+</section>
+</main>
+
+<footer>
+    <p><i>Nieuwsstraat 210<br>
+           3201 EE<br>
+           Spijkenisse</i>
+    </p>
+</footer>
+    
+</body>
+</html>
