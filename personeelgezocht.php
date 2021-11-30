c9060b98f17793df6ffe264b2dd96a6b6dd7d4b6
Robin <robinpeereboom04@gmail.com>
2021-11-30 11:14:04 +0100

    shabu

---
+++ personeelgezocht.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shabu to GO</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header class="banner">
+    <img src="image/logo.jpg" alt="logo" width="200" height="200">
+    <?php   include 'navigatie.html'; ?>
+  </header>
+
+<main>
+  <section>
+    <article>
+      <h1>
+        Welkom bij de medewerkers gezocht pagina!, wij zoeken JOU kom jij ons gezellige team versterken?
+      </h1>
+    </article>
+  </section>
+</main>
+    
+</body>
+</html>
