f874ad102756d575ab121c86149e319903e7c67a
Brandonvde010 <brandonvanderent010@gmail.com>
2021-11-30 11:03:44 +0100

    openingstijden gemaakt

--- openingstijden.php
+++ openingstijden.php
@@ -5,11 +5,95 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>openingstijden</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <header class="banner">
+    
         <img src="image/logo.jpg" alt="logo" width="200" height="200">
         <?php include 'navigatie.html'; ?>
     </header>
+<main>
+    <section>
+    <img src="image/6c2065f3-8020-4f51-ac6d-50d81ffdc8c2.png" alt="sushi" class="responsive">
+    <p> <h1>Openingstijden Shabu To Go:</h1></p>
+    <section class="tabel2">
+                <TABLE BORDER="1">
+                    <TR class="tabelrand">
+                        <TD>Maandag</TD>
+                        <TD>12:00 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Dinsdag</TD>
+                        <TD>12:00 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Woensdag</TD>
+                        <TD>12:00 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Donderdag</TD>
+                        <TD>12:00 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Vrijdag</TD>
+                        <TD>12:00 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Zaterdag</TD>
+                        <TD>12:00 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Zondag</TD>
+                        <TD>12:00 - 21:00</TD>
+                    </TR>
+                </TABLE>
+    </section>
+  
+    <section>
+    <p> <h1>Bezorgtijden Shabu To Go:</h1></p>
+    <section class="tabel2">
+                <TABLE BORDER="1">
+                    <TR class="tabelrand">
+                        <TD>Maandag</TD>
+                        <TD>16:45 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Dinsdag</TD>
+                        <TD>16:45 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Woensdag</TD>
+                        <TD>16:45 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Donderdag</TD>
+                        <TD>16:45 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Vrijdag</TD>
+                        <TD>16:45 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Zaterdag</TD>
+                        <TD>16:45 - 21:00</TD>
+                    </TR>
+                    <TR class="tabelrand">
+                        <TD>Zondag</TD>
+                        <TD>16:45 - 21:00</TD>
+                    </TR>
+                </TABLE>
+    </section>
+
+</main>
+
+<footer>
+    <p><i>Nieuwsstraat 210<br>
+        3201 EE<br>
+        Spijkenisse</i>
+    </p>
+  </footer>
+
 </body>
 </html>
