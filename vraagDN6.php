a5e7ce596a2c1bcab5b34991949598aff44a2abb
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2022-01-18 09:10:56 +0100

    Informatievraag / connectie met de database

---
+++ vraagDN6.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vrouwen</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header class="banner">
+        <img src="image/logo.jpg" alt="logo" width="200" height="200">
+        <?php   include 'navigatie.html'; ?>
+    </header>
+    <img src="image/ss_xmas_menu06.jpg" alt="sushi" class="responsive">
+
+
+<?php 
+//connection to a db// 
+    require_once("dbconshabu.php");
+
+    $query = $db->prepare("SELECT * FROM `client` WHERE gender LIKE '%female%' ORDER BY `client`.`clientid` ASC");
+    $query->execute();
+
+    $resultq = $query->fetchALL(PDO::FETCH_ASSOC);
+
+    foreach($resultq as $data) {
+    echo " KlantID : " .$data['clientid']; 
+    echo " Naam : ". $data['name'];
+    echo " telefoonnummer : ". $data['phone number'];
+    echo " leeftijd : ". $data['age'];
+    echo " Geslacht : ". $data['gender'];
+    echo " Plaats : ". $data['place'];
+    echo " Postcode : ". $data['postalcode'];
+    echo "<br>";  
+    }
+
+?>
+
+
+<footer>
+    <p><i>Nieuwsstraat 210<br>
+        3201 EE<br>
+        Spijkenisse</i>
+    </p>
+  </footer>
+
+</body>
+</html>
+
+</body>
+</html>
